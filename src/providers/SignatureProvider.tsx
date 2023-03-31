ae943a33130d0710abbdf5eb71ae55e98e0b9770
Eduardo Vargas <edvargast+git@gmail.com>
2023-03-30 22:23:14 -0400

    chore: update Provider with new types + example

--- src/providers/SignatureProvider.tsx
+++ src/providers/SignatureProvider.tsx
@@ -1,21 +1,42 @@
 import React, { createContext, FC, useContext, useState } from "react";
 
-interface SignatureContextProps {
-    imgUrl?: string;
-    setImgUrl: (inputString: string) => void;
+interface InputElementOptions {
+    textValue: string;
+    isBold: Boolean;
+    isItalics: Boolean;
 }
 
-const SignatureContext = createContext<SignatureContextProps>({} as SignatureContextProps);
-
-const SignatureProvider: FC = ({ children }) => {
+const createSignatureProviderState = () => {
     const [imgUrl, setImgUrl] = useState<string>();
+    const [name, setName] = useState<InputElementOptions>({ textValue: "", isBold: false, isItalics: false });
 
-    const singatureState = {
-        imgUrl,
-        setImgUrl,
+    /**
+     * Partial<> makes the types inside optional,
+     * meaning that updates can have any of the elements within the InputElementOptions interface but does NOT require them all
+     * @example updateName({isBold: true}) // this will only update the isBold key of the state object
+     */
+    const updateName = (updates: Partial<InputElementOptions>) => {
+        // spread (...) operator, copies (spreads) the object into a new one let a = { Hello: "world"}, let b = { ...a } will spread a into the new object that the curlies { } are creating
+        // if we spread multiple things, the second one will be spread on top of the first one
+        // ...name spreads the original state into the new object, ...updates spreads the contents on top of that one thus overriding (updating) the values
+        const copyOfName = { ...name, ...updates };
+
+        // after we have created the new object we call the state setter to update the state
+        setName(copyOfName);
     };
 
-    return <SignatureContext.Provider value={singatureState}>{children}</SignatureContext.Provider>;
+    return {
+        imgUrl,
+        setImgUrl,
+        updateName,
+    };
+};
+
+type SignatureContextProps = ReturnType<typeof createSignatureProviderState>;
+
+const SignatureContext = createContext<SignatureContextProps>({} as SignatureContextProps);
+const SignatureProvider: FC = ({ children }) => {
+    return <SignatureContext.Provider value={createSignatureProviderState()}>{children}</SignatureContext.Provider>;
 };
 
 const useSignatureContext = () => {
