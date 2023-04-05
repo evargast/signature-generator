915848c84e2bba6670492784996331b081b84a0c
Stefano Ramirez <ramirez.stefano@gmail.com>
2023-04-04 21:40:51 -0600

    refactor: optimized code and removed unused components and props
    re #31

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,24 +1,22 @@
 import { Flex } from "@adobe/react-spectrum";
 import { ToggleButton } from "@adobe/react-spectrum";
 import ClassicGridView from "@spectrum-icons/workflow/ClassicGridView";
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 
 interface Props {
     isToggled?: boolean;
 }
 
-const Header: FC<Props> = ({ isToggled }) => {
-    const [selected, setSelected] = React.useState(isToggled);
+const Header: FC<Props> = ({ isToggled = false }) => {
+    const [selected, setSelected] = useState(isToggled);
 
     return (
-        <>
-            <Flex direction="row" alignItems="baseline" justifyContent="space-between">
-                <h1>Signature Generator</h1>
-                <ToggleButton isQuiet onChange={setSelected} isSelected={selected}>
-                    <ClassicGridView />
-                </ToggleButton>
-            </Flex>
-        </>
+        <Flex direction="row" alignItems="baseline" justifyContent="space-between">
+            <h1>Signature Generator</h1>
+            <ToggleButton isQuiet onChange={setSelected} isSelected={selected}>
+                <ClassicGridView />
+            </ToggleButton>
+        </Flex>
     );
 };
 
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,11 +3,9 @@ import React, { FC } from "react";
 
 import { Header } from "../Header";
 
-interface Props {}
-
-const Layout: FC<Props> = ({ children }) => {
+const Layout: FC = ({ children }) => {
     return (
-        <Flex direction="column">
+        <Flex direction="column" marginX="size-200">
             <Header />
             {children}
         </Flex>
