c001dddcf7dda995b34b2b9daae85c8368461d5c
Stefano Ramirez <ramirez.stefano@gmail.com>
2023-04-12 12:28:22 -0600

    refactor: changed to flex wrapper, change to toggleButtons, labels rem
    re # 37

--- src/components/TextStyleOptions/TextStyleOptions.tsx
+++ src/components/TextStyleOptions/TextStyleOptions.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable no-console */
-import { ActionGroup, Item } from "@adobe/react-spectrum";
-import { Text } from "@adobe/react-spectrum";
-import type { Selection } from "@react-types/shared";
+// import { ActionGroup, Flex, Item, ToggleButton } from "@adobe/react-spectrum";
+// import { Text } from "@adobe/react-spectrum";
+import { Flex, ToggleButton } from "@adobe/react-spectrum";
+// import type { Selection } from "@react-types/shared";
 import TagBold from "@spectrum-icons/workflow/TagBold";
 import TagItalic from "@spectrum-icons/workflow/TagItalic";
 import React, { FC } from "react";
@@ -9,54 +10,67 @@ import React, { FC } from "react";
 export interface TextStyleProps {
     isBold?: boolean;
     isItalics?: boolean;
-    onChange?: (selectedKeys: (string | number)[]) => void;
+    onChange?: (options: { isBold?: boolean; isItalics?: boolean }) => void;
 }
 
-const TextStyleOptions: FC<TextStyleProps> = ({ isBold, isItalics, onChange }) => {
-    const [selected, setSelected] = React.useState<Selection>(() => {
-        const initialValue: string[] = [];
+// const TextStyleOptions: FC<TextStyleProps> = ({ isBold, isItalics, onChange }) => { //original
+const TextStyleOptions: FC<TextStyleProps> = ({ isBold, isItalics }) => {
+    // const [selected, setSelected] = React.useState<Selection>(() => {
+    //     const initialValue: string[] = [];
 
-        if (isBold) {
-            initialValue.push("bold");
-        }
+    //     if (isBold) {
+    //         initialValue.push("bold");
+    //     }
 
-        if (isItalics) {
-            initialValue.push("italics");
-        }
-        console.log(initialValue);
+    //     if (isItalics) {
+    //         initialValue.push("italics");
+    //     }
+    //     console.log(initialValue);
 
-        return new Set(initialValue);
-    });
+    //     return new Set(initialValue);
+    // });
 
-    const handleSelectionChange = (selectedKeys: Selection) => {
-        setSelected(selectedKeys);
-        if (onChange) {
-            const values: (string | number)[] = Array.from(selectedKeys);
-            onChange(values);
-        }
-    };
+    const [selectedBold, setSelectedBold] = React.useState(isBold);
+    const [selectedItalics, setSelectedItalics] = React.useState(isItalics);
+
+    // const handleSelectionChange = (selectedKeys: Selection) => {
+    //     setSelected(selectedKeys);
+    //     if (onChange) {
+    //         const values: (string | number)[] = Array.from(selectedKeys);
+    //         onChange(values);
+    //     }
+    // };
 
     return (
-        <div>
-            <ActionGroup
-                isEmphasized
-                density="compact"
-                isQuiet
-                selectionMode="multiple"
-                selectedKeys={selected}
-                onSelectionChange={handleSelectionChange}
-            >
-                <Item key="bold">
-                    <TagBold />
-                    <Text>Bold</Text>
-                </Item>
+        <Flex direction="row">
+            <ToggleButton isQuiet aria-label="BoldButton" isSelected={selectedBold} onChange={setSelectedBold}>
+                <TagBold />
+            </ToggleButton>
+            <ToggleButton isQuiet aria-label="ItalicsButton" isSelected={selectedItalics} onChange={setSelectedItalics}>
+                <TagItalic />
+            </ToggleButton>
 
-                <Item key="italics">
-                    <TagItalic />
-                    <Text>Italic</Text>
-                </Item>
-            </ActionGroup>
-        </div>
+            {/* <div>
+                <ActionGroup
+                    isEmphasized
+                    density="compact"
+                    isQuiet
+                    selectionMode="multiple"
+                    selectedKeys={selected}
+                    onSelectionChange={handleSelectionChange}
+                >
+                    <Item key="bold">
+                        <TagBold />
+                        <Text>Bold</Text>
+                    </Item>
+
+                    <Item key="italics">
+                        <TagItalic />
+                        <Text>Italic</Text>
+                    </Item>
+                </ActionGroup>
+            </div> */}
+        </Flex>
     );
 };
 
