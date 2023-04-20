acc6c7d481c5e0e0234fe0462e38bc0429b18101
Stefano Ramirez <ramirez.stefano@gmail.com>
2023-04-20 00:31:40 -0600

    refactor: changed div element for a Flex component
    re #44

--- src/components/TablePreview/TablePreview.tsx
+++ src/components/TablePreview/TablePreview.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@adobe/react-spectrum";
+import { Button, Flex } from "@adobe/react-spectrum";
 import { useSignatureContext } from "providers/SignatureProvider";
 import React, { FC, useRef } from "react";
 
@@ -24,41 +24,41 @@ const TablePreview: FC<Props> = ({}) => {
             selection.addRange(range);
             document.execCommand("copy");
             selection.removeAllRanges();
+            // eslint-disable-next-line no-console
+            console.log("Copied text!");
         }
     };
 
     return (
-        <>
-            <div style={{ display: "flex", flexDirection: "column" }}>
-                <style>{`table, td {border: 1px solid black;}`}</style>
-                <table ref={tableRef}>
-                    <tr>
-                        <td rowSpan={3}>Image Taking 3 cells</td>
-                        <td
-                            style={{
-                                fontWeight: name.cssBold,
-                                fontStyle: name.cssItalics,
-                            }}
-                        >
-                            {name.textValue}
-                        </td>
-                        <td>Phone Number</td>
-                    </tr>
-                    <tr>
-                        <td>Title</td>
-                        <td>Email</td>
-                    </tr>
-                    <tr>
-                        <td>Company</td>
-                        <td>LinkedIn</td>
-                    </tr>
-                </table>
+        <Flex direction="column">
+            <style>{`table, td {border: 1px solid black;}`}</style>
+            <table ref={tableRef}>
+                <tr>
+                    <td rowSpan={3}>Image Taking 3 cells</td>
+                    <td
+                        style={{
+                            fontWeight: name.cssBold,
+                            fontStyle: name.cssItalics,
+                        }}
+                    >
+                        {name.textValue}
+                    </td>
+                    <td>Phone Number</td>
+                </tr>
+                <tr>
+                    <td>Title</td>
+                    <td>Email</td>
+                </tr>
+                <tr>
+                    <td>Company</td>
+                    <td>LinkedIn</td>
+                </tr>
+            </table>
 
-                <Button marginTop="size-150" variant="accent" width="100%" onPress={handleCopy}>
-                    Copy
-                </Button>
-            </div>
-        </>
+            <Button marginTop="size-150" variant="accent" width="100%" onPress={handleCopy}>
+                Copy
+            </Button>
+        </Flex>
     );
 };
 export default TablePreview;
