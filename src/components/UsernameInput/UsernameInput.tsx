9ca7cf34b0fd7d81b7506b365e59dea8f354fce9
Stefano Ramirez <ramirez.stefano@gmail.com>
2023-05-06 22:28:27 -0600

    feat: added the email field regex check for an email format string
    re #61

--- src/components/UsernameInput/UsernameInput.tsx
+++ src/components/UsernameInput/UsernameInput.tsx
@@ -1,8 +1,7 @@
-import { TextField } from "@adobe/react-spectrum";
-import { Flex } from "@adobe/react-spectrum";
+import { Flex, TextField } from "@adobe/react-spectrum";
 import { Color } from "@react-types/color";
-import { InputElementOptions } from "providers/SignatureProvider";
-import React, { FC } from "react";
+import { InputElementOptions, useSignatureContext } from "providers/SignatureProvider";
+import React, { FC, useEffect, useState } from "react";
 
 import { TextStyleOptions, TextStyleProps } from "./../TextStyleOptions";
 
@@ -13,6 +12,20 @@ interface UsernameInputProps extends Omit<TextStyleProps, "onColorChange"> {
 }
 
 const UsernameInput: FC<UsernameInputProps> = ({ state, onInputChange, label }) => {
+    const [emailValidation, setEmailValidation] = useState<"valid" | "invalid">();
+    const emailRegex = new RegExp(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/);
+    const { email } = useSignatureContext();
+
+    useEffect(() => {
+        if (email !== undefined) {
+            validateEmail(email.textValue);
+        }
+    }, [email]);
+
+    const validateEmail = (input: string) => {
+        setEmailValidation(emailRegex.test(input) ? "valid" : "invalid");
+    };
+
     const handleButtonChange = (options: { isBold?: boolean; isItalics?: boolean }) => {
         // Since the provider needs a variant to know where to apply the logic, we are adding it to the options object
         const modifiedOptions = { ...options, variant: state.variant };
@@ -30,7 +43,13 @@ const UsernameInput: FC<UsernameInputProps> = ({ state, onInputChange, label }) 
 
     return (
         <Flex gap="size-200" alignItems="end" direction="row">
-            <TextField label={label} onChange={handleInputChange} value={state.textValue} width="size-3600" />
+            <TextField
+                label={label}
+                onChange={handleInputChange}
+                value={state.textValue}
+                width="size-3600"
+                validationState={state.variant === "email" ? emailValidation : undefined}
+            />
             <TextStyleOptions
                 onChange={handleButtonChange}
                 onColorChange={handleColorChange}
